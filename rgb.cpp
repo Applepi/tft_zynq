968b4db8d661756cab4898e62d486c09f6f9415d
notaUser <Applepi@users.noreply.github.com>
2019-10-01 14:08:27 -0500

    Update rgb.cpp

--- rgb.cpp
+++ rgb.cpp
@@ -15,28 +15,28 @@ int main()
 	int y = 480; // 0 - 479 0x1E0
 	int addr = 0;
 	int data[0x5DC00];
-	srand(time(NULL));
+	srand(time(NULL)); // bloat
 	for (int i=0; i < y; i++)
 	{
 		for (int j=0; j < x; j++)
 		{
-			r = rand()%31;
-			g = rand()%63;
-			b = rand()%31;
+			r = rand()%31; // needs better solution
+			g = rand()%63; // "
+			b = rand()%31; // "
 			//r = 0b11111;
 			//g = 0b111111;
 			//b = 0b11111;
 			addr++;
 			int buffer = 0;
-			buffer = ((((buffer ^ r) << 6) ^ g )<< 5 )^ b;
-			cout << bitset <16> (buffer) << endl;
+			buffer = ((((buffer ^ r) << 6) ^ g )<< 5 )^ b; // bitsift concat into buffer
+			cout << bitset <16> (buffer) << endl; // debug output 
 			data[j] = buffer;
-			cout << data[j] << endl;
+			cout << data[j] << endl; // debug output
 		}
 
 
 	}
 
-	cout << addr << endl;
+	cout << addr << endl; // final addr debug output
 	return 0; 
 }
